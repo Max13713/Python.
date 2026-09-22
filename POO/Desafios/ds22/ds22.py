a9b9447a110b7c11896b93e661e2e66bddae66c7
Max <paulo.santos27032013@gmail.com>
2026-09-22 15:26:58 -0300

    ds22 feito

--- POO/Desafios/ds22/ds22.py
+++ POO/Desafios/ds22/ds22.py
@@ -1,4 +1,77 @@
-class Caneta:
-    pass
+import math
+from rich import print
+from rich.panel import Panel
+class ControleRemoto:
+    canal_min:int = 1
+    canal_max:int = 5
+    volume_min:int = 1
+    volume_max:int = 5
+    def __init__(self, canal=1, volume=2, ):
+        self.canal_atual:int = canal
+        self.volume_atual:int = volume
+        self.ligado:bool = False
 
-c1 = Caneta()
+    def liga_desliga(self):
+        self.ligado = not self.ligado
+
+    def canal_mais(self):
+        if self.ligado:
+            if self.canal_atual == ControleRemoto.canal_max:
+                self.canal_atual = ControleRemoto.canal_min
+            else:
+                self.canal_atual += 1
+
+    def canal_menos(self):
+        if self.ligado:
+            if self.canal_atual == ControleRemoto.canal_min:
+                self.canal_atual = ControleRemoto.canal_max
+            else:
+                self.canal_atual -= 1
+
+    def volume_mais(self):
+        if self.ligado:
+            if self.volume_atual != ControleRemoto.volume_max:
+                self.volume_atual += 1
+
+    def volume_menos(self):
+        if self.ligado:
+            if self.volume_atual != ControleRemoto.volume_min:
+                self.volume_atual -= 1
+
+    def mostra_tv(self):
+        conteudo = ''
+        if not self.ligado:
+            conteudo = ':no_entry_sign: [red]A TV esta desligada[/]'
+        else:
+            conteudo = f'CANAL  = '
+            for canal in range(ControleRemoto.canal_min, ControleRemoto.canal_max+1):
+                if canal == self.canal_atual:
+                    conteudo += f'[yellow on yellow] {canal} [/]'
+                else:
+                    conteudo += f' {canal} '
+            conteudo += f'\nVOLUME = '
+            for volume in range(ControleRemoto.volume_min, ControleRemoto.volume_max+1):
+                if volume <= self.volume_atual:
+                    conteudo += f'[black on cyan] [/]'
+                else:
+                    conteudo += f'[black on white] [/]'
+        tv = Panel(conteudo, title='[ TV ]', width=35)
+        print(tv)
+c1 = ControleRemoto(1, 4)
+while True:
+    c1.mostra_tv()
+    commando = str(input(f' < CH{c1.canal_atual}  >    - VOL{c1.volume_atual} + '))
+    match commando:
+        case '0':
+            break
+        case '@':
+            c1.liga_desliga()
+        case '>':
+            c1.canal_mais()
+        case '<':
+            c1.canal_menos()
+        case '+':
+            c1.volume_mais()
+        case '-':
+            c1.volume_menos()
+    print('\n' * 10)
